4fb4dea8082695a0db35e130eac87cfb60d1a429
Félix Díez <felixeu31@gmail.com>
2022-12-17 10:59:43 +0100

    feat: benchmark for any vs count

---
+++ Benchmarks/BasicBenchmark/BasicBenchmark/ListOperationsBenchmarks.cs
@@ -0,0 +1,46 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace BasicBenchmark
+{
+    [MemoryDiagnoser()]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn()]
+    public class ListOperationsBenchmarks
+    {
+        private readonly List<int> _list;
+        private readonly IEnumerable<int> _enumerable;
+
+        public ListOperationsBenchmarks()
+        {
+            Random rand = new Random();
+            _enumerable = Enumerable.Range(0, 10000).Select(x => rand.Next(10000));
+            _list = _enumerable.ToList();
+        }
+
+        [Benchmark]
+        public void ListAny()
+        {
+            var any = _list.Any();
+        }
+        
+        [Benchmark]
+        public void ListCount()
+        {
+            var any = _list.Count > 0;
+        }
+
+
+        [Benchmark]
+        public void EnumerableAny()
+        {
+            var any = _enumerable.Any();
+        }
+
+        [Benchmark]
+        public void EnumerableCount()
+        {
+            var any = _enumerable.Count() > 0;
+        }
+    }
+}
---
+++ Benchmarks/BasicBenchmark/BasicBenchmark/Program.cs
@@ -0,0 +1,4 @@
+﻿using BasicBenchmark;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<ListOperationsBenchmarks>();
